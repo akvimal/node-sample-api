37a796f9bd1193fac4ff327a1820d3f9b64b02d9
Vimal Krishnan <akvimal@gmail.com>
2021-05-04 22:52:59 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
+
+const app = express()
+const PORT = process.env.APP_PORT || 3000
+
+app.get('/', (req,res)=>{
+    res.send({greeting:'Hello API'})
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server is listening on port ${PORT} ...`)
+})
